40467e6d55950333d8429f70ac6faf25f288874f
suh <125309833+interfluve-wav@users.noreply.github.com>
2026-04-16 14:29:22 -0400

    v0.1.3: src/auto_captcha/__init__.py

--- src/auto_captcha/__init__.py
+++ src/auto_captcha/__init__.py
@@ -3,5 +3,5 @@
 from .solver import CaptchaSolver, CaptchaResult
 from .wrapper import SmartPage, smart_page
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = ["CaptchaSolver", "CaptchaResult", "SmartPage", "smart_page"]
